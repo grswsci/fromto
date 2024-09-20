5fbbaf3311f367993d9a9bc2ffffe24225a6712f
grswsci <ahmulyy@163.com>
2024-09-21 01:19:37 +0800

    Initial commit

---
+++ R/fromto.R
@@ -0,0 +1,24 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+fromto <- function(genes,from,to){
+  data_file = system.file("data", "trans2gene.RDS", package = "fromto")
+  Gene_all = readRDS("/fromto/data/trans2gene.RDS")
+  Gene_intersect = intersect(genes,Gene_all[,from])
+  Gene_subset = Gene_all[which(Gene_all[,from] %in% Gene_intersect),]
+  Gene_output = Gene_subset[,c(from,to)]
+  return(Gene_output)
+}
+colnames(Gene_all)
---
+++ R/zzz.R
@@ -0,0 +1,13 @@
+.onLoad <- function(libname, pkgname){
+  msg <- paste0(pkgname, " v", "0.1.0", "\n\n","For help: https://grswsci.top/","\n\n")
+  citation <- paste0("If you use ", pkgname, " in published research, please thank:\n",
+                      "Yuyao Liu, Bioinformatics R&D Department, Hefei GuangRe Biotechnology Co., Ltd, Hefei, China","\n\n",
+                      "You can freely convert the following GeneIDs!","\n",
+                      "Symbol" "\n","NCBI_GeneID""\n","HGNC_GeneID","\n","SwissProt_GeneID","\n",
+                      "Ensembl_GeneID","\n","OMIM_GeneID","\n",
+                      "Proteins_GeneID","\n","Description_GeneID","\n",
+                      "Transcripts_GeneID","\n","Gene_Group_Identifier_GeneID""\n",
+                      "Synonyms_GeneID")
+  packageStartupMessage(paste0(msg, citation))
+}
+
