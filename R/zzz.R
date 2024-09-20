1353f34e5d8fb9163b945b80e5fdbf292ed4c6ef
grswsci <ahmulyy@163.com>
2024-09-21 01:30:20 +0800

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -3,7 +3,7 @@
   citation <- paste0("If you use ", pkgname, " in published research, please thank:\n",
                       "Yuyao Liu, Bioinformatics R&D Department, Hefei GuangRe Biotechnology Co., Ltd, Hefei, China","\n\n",
                       "You can freely convert the following GeneIDs!","\n",
-                      "Symbol" "\n","NCBI_GeneID""\n","HGNC_GeneID","\n","SwissProt_GeneID","\n",
+                      "Symbol","\n","NCBI_GeneID""\n","HGNC_GeneID","\n","SwissProt_GeneID","\n",
                       "Ensembl_GeneID","\n","OMIM_GeneID","\n",
                       "Proteins_GeneID","\n","Description_GeneID","\n",
                       "Transcripts_GeneID","\n","Gene_Group_Identifier_GeneID""\n",
