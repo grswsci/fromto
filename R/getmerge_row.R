9c7c130434ac1f411fc46e1b408d30862b74b62c
grswsci <ahmulyy@163.com>
2024-10-11 06:55:42 +0800

    grswsci

--- R/getmerge_row.R
+++ R/getmerge_row.R
@@ -5,3 +5,15 @@ getmerge_row <- function(data1,data2){
   data3 = cbind(data1,data2)
   return(data3)
 }
+
+getsame_row_data1 <- function(data1,data2){
+  samesample = intersect(rownames(data1),rownames(data2))
+  data1 = data1[samesample,,drop=FALSE]
+  return(data1)
+}
+
+getsame_row_data2 <- function(data1,data2){
+  samesample = intersect(rownames(data1),rownames(data2))
+  data2 = data2[samesample,,drop=FALSE]
+  return(data2)
+}
