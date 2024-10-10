8c5bb9278406318363cfe09339c0403508bd7fd3
grswsci <ahmulyy@163.com>
2024-10-11 07:51:37 +0800

    Update geocli.R

--- R/geocli.R
+++ R/geocli.R
@@ -1,6 +1,11 @@
 geocli <- function(GEO_ID){
   clinicallines = readLines(paste0(GEO_ID,"_series_matrix.txt"))
   clinicallines_filtered = clinicallines[substr(clinicallines,1,7)=="!Sample"]
+  contains_colon = function(column) {
+    any(grepl(": ", as.character(column)))
+  }
+  clinicallines_filtered_with_colon_index = which(sapply(clinicallines_filtered, contains_colon))
+  clinicallines_filtered =  clinicallines_filtered[clinicallines_filtered_with_colon_index]
   clinicaldata = read.table(textConnection(paste(clinicallines_filtered, collapse="\n")), header=TRUE)
   clinical = t(clinicaldata)
 
@@ -8,14 +13,10 @@ geocli <- function(GEO_ID){
   colnames(clinical) = clinical[1,]
   clinical = as.data.frame(clinical[-1,-1])
 
-  contains_colon = function(column) {
-    any(grepl(": ", as.character(column)))
-  }
   cols_with_colon_index = which(sapply(clinical, contains_colon))
-
   clinical = clinical[,cols_with_colon_index]
-  colnames(clinical) = NULL
 
+  colnames(clinical) = NULL
   for (col in 1:ncol(clinical)) {
     split_names = unique(sapply(strsplit(clinical[,col], ": ", fixed = TRUE), function(x) c(x[1])))
     print(split_names)
