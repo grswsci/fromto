7e96222b9219d21c9970f32d8b18e1f781ff4030
grswsci <ahmulyy@163.com>
2024-10-11 10:19:52 +0800

    Update counts2tpm.R

--- R/counts2tpm.R
+++ R/counts2tpm.R
@@ -1,4 +1,5 @@
 counts2tpm <- function(counts){
+  library(limma)
   data_file = system.file("data", "Human.GRCh38.p13.annot.RDS", package = "fromto")
   Gene_length = readRDS(data_file)
   Gene_length = as.data.frame(Gene_length)
@@ -7,6 +8,7 @@ counts2tpm <- function(counts){
   row.names(Gene_length) = Gene_length[,1]
   Gene_length_counts = getmerge_row(Gene_length,counts)
   Gene_length_counts = Gene_length_counts[,-1]
+  Gene_length_counts = as.matrix(Gene_length_counts)
   class(Gene_length_counts) = "numeric"
   Gene_length_counts = as.data.frame(Gene_length_counts)
 
