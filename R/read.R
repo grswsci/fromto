04c5138d751dd899d1909cdf0ef5d8625a18beb8
grswsci <ahmulyy@163.com>
2024-10-14 11:19:07 +0800

    Update read.R

--- R/read.R
+++ R/read.R
@@ -1,7 +1,7 @@
 read_fromto <- function(file,row_names = TRUE){
   data = data.table::fread(file)
   data = as.data.frame(data)
-  if(row_names = TRUE){
+  if(row_names == TRUE){
     if(length(unique(data[,1])) < nrow(data)){
       data = as.matrix(data)
       rownames(data) = data[,1]
