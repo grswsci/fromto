ae22264e6388dce16345c12594f97a5a9f6c253d
grswsci <ahmulyy@163.com>
2024-10-24 01:25:21 +0800

    Update uplot.R

--- R/uplot.R
+++ R/uplot.R
@@ -28,54 +28,64 @@ uplot_cell = function(scRNA, plotname = "CellType", reduction = "umap", label = 
   library(tidyverse)
   library(ggrepel)
   umap = scRNA@reductions[[reduction]]@cell.embeddings %>% as.data.frame() %>% cbind(Cluster = scRNA@meta.data[,label])
+  cell_type_med = umap %>% group_by(Cluster) %>% summarise(UMAP_1 = median(UMAP_1),UMAP_2 = median(UMAP_2))
+  UMAP_1_min = min(umap$UMAP_1)
+  UMAP_2_min = min(umap$UMAP_2)
   colnames(umap) = c("UMAP_1","UMAP_2","Cluster")
   p = ggplot(umap,aes(x= UMAP_1 , y = UMAP_2 ,color = Cluster)) +
     geom_point(size = 0.1 , alpha = alphas)  +
-    scale_color_manual(values = mycolor)
-  p2 = p + theme(panel.grid.major = element_blank(),
-                 panel.grid.minor = element_blank(),
-                 panel.border = element_blank(),
-                 axis.title = element_blank(),
-                 axis.text = element_blank(),
-                 axis.ticks = element_blank(),
-                 panel.background = element_rect(fill = 'white'),
-                 plot.background=element_rect(fill="white"))
-  p3 = p2 + theme(legend.title = element_blank(),
-                legend.key=element_rect(fill='white'),
-                legend.text = element_text(size=20),
-                legend.key.size=unit(1,'cm') ) +
-  guides(color = guide_legend(override.aes = list(size=5)))
-  p4 = p3 + geom_segment(aes(x = min(UMAP_1) , y = min(UMAP_2),
-                         xend = min(UMAP_1) +3,
-                         yend = min(UMAP_2) ),
-                         colour = "black", size=1,
-                         arrow = arrow(length = unit(0.3,"cm")))+
-  geom_segment(aes(x = min(UMAP_1)  ,
-                   y = min(UMAP_2),
-                   xend = min(UMAP_1) ,
-                   yend = min(UMAP_2) + 3),
-               colour = "black", size=1,
+    scale_color_manual(values = mycolor) +
+    theme(panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          panel.border = element_blank(),
+          axis.title = element_blank(),
+          axis.text = element_blank(),
+          axis.ticks = element_blank(),
+          panel.background = element_rect(fill = 'white'),
+          plot.background = element_rect(fill="white")
+          ) +
+    theme(legend.title = element_blank(),
+          legend.key = element_rect(fill='white'),
+          legend.text = element_text(size=20),
+          legend.key.size = unit(1,'cm')
+          ) +
+  guides(color = guide_legend(override.aes = list(size = 5))
+         ) +
+    annotate("segment", x = UMAP_1_min ,
+                     y = UMAP_2_min,
+                     xend = UMAP_1_min + 3,
+                     yend = UMAP_2_min,
+                     colour = "black",
+                     size = 1,
+                     arrow = arrow(length = unit(0.3,"cm"))
+                 ) +
+    annotate("segment", x = UMAP_1_min,
+                   y = UMAP_2_min,
+                   xend = UMAP_1_min ,
+                   yend = UMAP_2_min + 3,
+                   colour = "black",
+                   size = 1,
                arrow = arrow(length = unit(0.3,"cm"))) +
   annotate("text",
-           x = min(UMAP_1) + 1.5,
-           y = min(UMAP_2) -1,
+           x = UMAP_1_min + 1.5,
+           y = UMAP_2_min - 1,
            label = "UMAP_1",
            color = "black",
            size = 3,
            fontface = "bold" ) +
   annotate("text",
-           x = min(UMAP_1) - 1,
-           y = min(UMAP_2) + 1.5,
+           x = UMAP_1_min - 1,
+           y = UMAP_2_min + 1.5,
            label = "UMAP_2",
-           color = "black",size = 3,
-           fontface = "bold" ,angle = 90)
-  cell_type_med = umap %>% group_by(Cluster) %>%
-    summarise(UMAP_1 = median(UMAP_1),UMAP_2 = median(UMAP_2))
-  p6 = p4 + geom_label_repel(aes(label = Cluster),
-                          fontface="bold",
+           color = "black",
+           size = 3,
+           fontface = "bold" ,angle = 90) +
+    geom_label_repel(aes(label = Cluster),
+                          fontface = "bold",
                           data = cell_type_med,
-                          point.padding=unit(0.5, "lines")) +
+                          point.padding = unit(0.5, "lines")
+                     ) +
   theme(legend.position = "none")
-  ggsave(paste0(plotname,".pdf"), plot = p6, width = 6, height = 6)
-  return(p6)
+  ggsave(paste0(plotname,".pdf"), plot = p, width = 6, height = 6)
+  return(p)
 }
