d7b3a3540d183c02d01c4dea57b1c293d5c48925
grswsci <ahmulyy@163.com>
2024-12-05 18:45:22 +0800

    grswsci

--- GPL_Deal.R
+++ GPL_Deal.R
@@ -1,6 +1,7 @@
 library(fromto)
-gpl = read_fromto("/fromto/GPL15207-17536.txt",row_names = FALSE)
-gpl = gpl[,c(1,17)]
+gpl = read_fromto("/fromto/GPL22120-25936.txt",row_names = FALSE)
+gpl = gpl[,c(1,10)]
 colnames(gpl) = c("ID","Gene Symbol")
 gpl = gpl[which(gpl$`Gene Symbol` != ""),]
-saveRDS(gpl,"/fromto/data/GPL15207.RDS")
+gpl = gpl[which(gpl$`Gene Symbol` != "-"),]
+saveRDS(gpl,"/fromto/data/GPL22120.RDS")
--- GPL_Deal_strsplit.R
+++ GPL_Deal_strsplit.R
@@ -1,19 +1,16 @@
+#devtools::install_github("grswsci/fromto")
 library(fromto)
 gpl = read_fromto("/fromto/GPL23159-184565.txt",row_names = FALSE)
 gpl = gpl[,c(1,10)]
 gpl$hgnc_id = stringr::str_extract(gpl$SPOT_ID, "(?<=HGNC:)[0-9]+")
 Symbol = fromto(genes = gpl$hgnc_id, from = "HGNC_GeneID", to = "Symbol")
 
-add_line = c()
-for (variable in gpl$hgnc_id) {
-  if(variable %in% Symbol$HGNC_GeneID){
-    Symbol_subset = Symbol[which(Symbol$HGNC_GeneID == variable),]
-    add_line = c(add_line,Symbol_subset$Symbol)
-  }else{
-    add_line = c(add_line,NA)
-  }
-}
-gpl_new = cbind(gpl,add_line)
+gpl_new = merge_col_add(data1 = gpl,
+                    data2 = Symbol,
+                    data1_var = "hgnc_id",
+                    data2_var_same_data1 = "HGNC_GeneID",
+                    data2_var_add_data1 = "Symbol")
+
 gpl_new = gpl_new[,c(1,4)]
 colnames(gpl_new) = c("ID","Gene Symbol")
 gpl_new = gpl_new[which(gpl_new$`Gene Symbol` != ""),]
