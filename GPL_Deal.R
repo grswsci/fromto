8f9684020df6dd87427db5b3d31e5091b5970f80
grswsci <ahmulyy@163.com>
2024-12-05 17:29:32 +0800

    grswsci

--- GPL_Deal.R
+++ GPL_Deal.R
@@ -1,6 +1,6 @@
 library(fromto)
-gpl = read_fromto("/fromto/GPL7191.txt",row_names = FALSE)
-gpl = gpl[,c(1,19)]
+gpl = read_fromto("/fromto/GPL15207-17536.txt",row_names = FALSE)
+gpl = gpl[,c(1,17)]
 colnames(gpl) = c("ID","Gene Symbol")
 gpl = gpl[which(gpl$`Gene Symbol` != ""),]
-saveRDS(gpl,"/fromto/data/GPL7191.RDS")
+saveRDS(gpl,"/fromto/data/GPL15207.RDS")
